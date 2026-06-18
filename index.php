b49d0cd9ddffb070c1877b2e46037dfc141b9ecc
Vasyl Sokolyk <vsokolyk@gmail.com>
2026-06-18 18:35:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,17 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>TEST WebHook</title>
+
+    <script type="text/javascript">
+      window.helpcrunchSettings = {
+        organization: 'jazzman',
+        appId: '5595e1ca-fb3e-46da-8134-425a742372f3',
+      };
+    </script>
+
+    <script type="text/javascript">
+      (function(w,d){var hS=w.helpcrunchSettings;if(!hS||!hS.organization){return;}var widgetSrc='https://embed.helpcrunch.com/sdk.js';w.HelpCrunch=function(){w.HelpCrunch.q.push(arguments)};w.HelpCrunch.q=[];function r(){if (d.querySelector('script[src="' + widgetSrc + '"')) { return; }var s=d.createElement('script');s.async=1;s.type='text/javascript';s.src=widgetSrc;(d.body||d.head).appendChild(s);}if(d.readyState === 'complete'||hS.loadImmediately){r();} else if(w.attachEvent){w.attachEvent('onload',r)}else{w.addEventListener('load',r,false)}})(window, document)
+    </script>
 </head>
 <body>
   <h1>TEST WebHook</h1>
